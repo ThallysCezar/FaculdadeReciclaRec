a0a52e81c6718622f3de7df7b709daa73374626b
Thallys Cezar <thalys_cezar@hotmail.com>
2022-10-11 08:05:32 -0300

    refactor: reconfig img MapaReciclagem

--- src/components/MapaReciclagem/index.tsx
+++ src/components/MapaReciclagem/index.tsx
@@ -1,17 +1,68 @@
+import "./style.css";
 
-const mapa = require("../../assets/mapa.png") as string;
+const lixeira =
+  require("../../assets/img/MapaReciclagemImg/lixeira.png") as string;
+const recicla =
+  require("../../assets/img/MapaReciclagemImg/recicla.png") as string;
+const caminhao =
+  require("../../assets/img/MapaReciclagemImg/caminhao.png") as string;
+const consumidor =
+  require("../../assets/img/MapaReciclagemImg/consumidor.png") as string;
 
-const styleImg = {
-    width: "800px",
-    heitgh: "800px"
-}
+const imgs = [
+  {
+    id: 1,
+    caption: "Coleta e Triagem",
+    path: caminhao,
+    alt: "imagem do caminaho do lixo",
+  },
+  {
+    id: 2,
+    caption: "Reciclagem",
+    path: lixeira,
+    alt: "imagem da lixeira",
+  },
+  {
+    id: 3,
+    caption: "Distribuição",
+    path: recicla,
+    alt: "imagem da reciclagem",
+  },
+  {
+    id: 4,
+    caption: "Consumidor",
+    path: consumidor,
+    alt: "imagem do consumidor",
+  },
+];
 
 export const MapaReciclagem = () => {
-    return(
-        <div className="container">
-            <figure>
-                <img src={mapa} className="mx-auto d-block" alt="Mapa Reciclagem" style={styleImg}/>
-            </figure>
+  return (
+    <div className="container">
+      <div className="row">
+        <h2 className="text-center mt-2"> Mapa da Reciclagem</h2>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <div className="row justify-content-center">
+        {imgs.map((item) => {
+          return (
+              <div className="col-md-auto">
+                <figure>
+                  <img
+                    src={item.path}
+                    alt={item.alt}
+                    className="mx-auto d-block styleImg"
+                  />
+                  <p className="text-center">{item.caption}</p>
+                </figure>
+              </div>
+          );
+        })}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
